3717c085349edd4c73496caf9a50b70a0718583b
p8319514-spec <p8319514@gmail.com>
2026-04-17 19:50:30 +0530

    Change database name and modify exports
    Updated database name from 'dealerDB' to 'giftdb' and removed client export.

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 const { MongoClient } = require("mongodb");
 
 const uri = "mongodb://localhost:27017";
-
 const client = new MongoClient(uri);
 
 async function connectToDatabase() {
   await client.connect();
-  return client.db("dealerDB");
+  return client.db("giftdb");
 }
 
-module.exports = { connectToDatabase, client };
+module.exports = { connectToDatabase };
