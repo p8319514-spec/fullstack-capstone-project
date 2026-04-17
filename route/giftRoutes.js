7f2414a5c2222b5f00414763d2f052d0d029d19b
p8319514-spec <p8319514@gmail.com>
2026-04-17 19:51:55 +0530

    Update giftRoutes.js

--- route/giftRoutes.js
+++ route/giftRoutes.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { connectToDatabase } = require('../db');
 
-function connectToDatabase() {
-  console.log("Database connected");
-}
-
-connectToDatabase();
-
-router.get('/api/gifts', (req, res) => {
-  res.send("All gifts");
+router.get('/api/gifts', async (req, res) => {
+  const db = await connectToDatabase();
+  const data = await db.collection('gifts').find().toArray();
+  res.json(data);
 });
 
-router.get('/api/gifts/:id', (req, res) => {
-  res.send("Gift by ID");
+router.get('/api/gifts/:id', async (req, res) => {
+  const db = await connectToDatabase();
+  const data = await db.collection('gifts').findOne({
+    id: parseInt(req.params.id)
+  });
+  res.json(data);
 });
 
 module.exports = router;
